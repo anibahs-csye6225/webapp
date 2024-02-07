ea093cd5e90f6127a454d771090d08c5816bb736
Shabina Singh <anibahs@gmail.com>
2024-02-06 22:05:10 -0500

    changes for /v1/user, /v1/user/self

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // app.js - entry point
 const express = require('express');
-const router = require('./src/routes/healthcheck.router.js');
+const db = require('./database.js')
+const hc_router = require('./src/routes/healthcheck.router.js');
+const auth_router = require('./src/routes/auth.router.js');
+
 const app = express();
 
 //standard header settings on all API requests
@@ -14,18 +17,24 @@ app.use(express.json(), function(req, res, next) {
 });
 
 //router for API Endpoint for health check
-app.use('/healthz', router);
+app.use('/healthz', hc_router);
+
+//router for API Endpoints for authentication
+app.use('/v1/user', auth_router);
+
 
 //function for all other Endpoints
-//returns standard 404 HTTP status code for unrecongnised Endpoints
+//returns standard 404 HTTP status code for unrecognised Endpoints
 app.use('/', function(req, res) {
     console.error('ERROR: Endpoint not found')
     res.status(404).end();
 });
 
+
 const PORT = process.env.SERVER_PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
 
 module.exports = app;
