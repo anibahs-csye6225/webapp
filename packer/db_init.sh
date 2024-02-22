b95fe72adcd80e299a8b89c96b39a8c4d6021de9
Shabina Singh <37799489+anibahs@users.noreply.github.com>
2024-02-21 20:37:41 -0500

    Assignment4 (#12)
    * adding draft service file
    
    * add packer files
    
    * added environment initialization
    
    * added modifications for git workflow
    
    * Update db_init.sh
    
    * merge
    
    * comment

--- packer/db_init.sh
+++ packer/db_init.sh
@@ -10,7 +10,9 @@ sudo -i -u postgres psql -t -P format=unaligned -c 'show hba_file';
 PG_CONFIG=$(sudo -i -u postgres psql -t -P format=unaligned -c 'show hba_file';)
 echo "$PG_CONFIG"
 # PG_CONFIG="/var/lib/pgsql/data/pg_hba.conf"
-echo "host    all             postgres        127.0.0.1/32            trust" | sudo tee -a "$PG_CONFIG"
+#echo "host    all             postgres        127.0.0.1/32            trust" | sudo tee -a "$PG_CONFIG"
+sudo sed -i 's/ident/trust/g' "$PG_HBA_CONF"
+
 sudo tail "$PG_CONFIG"
 sudo systemctl stop postgresql
 sudo systemctl start postgresql
