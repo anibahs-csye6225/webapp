e284c1b9aae9b03f3692ee88333fe144a0013ffc
Shabina Singh <anibahs@gmail.com>
2024-03-30 12:46:13 -0400

    edit

--- packer/ops.sh
+++ packer/ops.sh
@@ -12,6 +12,3 @@ sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 # sudo mkdir -p /etc/google-cloud-ops-agent
 
 sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
-sudo systemctl restart google-cloud-ops-agent
-
-sudo systemctl status google-cloud-ops-agent"*" --no-pager
