abea464a708579dc9973621ceefb0d3dab9ea1c8
Shabina Singh <anibahs@gmail.com>
2024-03-30 12:08:09 -0400

    edit

--- packer/ops.sh
+++ packer/ops.sh
@@ -9,7 +9,7 @@ sudo chmod 755 -R "$LOG_DIR"
 
 sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
-sudo mkdir -p /etc/google-cloud-ops-agent
+# sudo mkdir -p /etc/google-cloud-ops-agent
 
 sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl restart google-cloud-ops-agent
