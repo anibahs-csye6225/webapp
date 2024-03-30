672525d2a5d779495591105d3d7837c7aeca7242
Shabina Singh <anibahs@gmail.com>
2024-03-30 11:45:04 -0400

    restructure ops agent scripts

--- packer/ops.sh
+++ packer/ops.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+LOG_DIR="/var/log/webapp"
 
-sudo mkdir /etc/google-cloud-ops-agent
-#sudo touch /etc/google-cloud-ops-agent/config.yaml
-sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
+
+sudo mkdir "$LOG_DIR"
+sudo chown -R csye6225:csye6225 "$LOG_DIR"
+sudo chmod 755 -R "$LOG_DIR"
+
+
+# sudo mkdir /etc/google-cloud-ops-agent
+# sudo touch /etc/google-cloud-ops-agent/config.yaml
+# sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
 sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl restart google-cloud-ops-agent
 
